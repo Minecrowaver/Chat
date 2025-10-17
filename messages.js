23097f582153e7b2d2a69629d1e2d778c8210862
Minecrowaver <xingrui.chen@student.keystoneacademy.cn>
2025-10-17 14:56:59 +0800

    Create messages.js

---
+++ messages.js
@@ -0,0 +1,16 @@
+[
+  {
+    "id": 1,
+    "sender": "System",
+    "content": "Welcome to Minecrowaver's GitHub Chat!",
+    "time": "10:00 AM",
+    "timestamp": 1700000000000
+  },
+  {
+    "id": 2,
+    "sender": "System", 
+    "content": "Messages are stored in this GitHub repository.",
+    "time": "10:01 AM",
+    "timestamp": 1700000001000
+  }
+]
